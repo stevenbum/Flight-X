30d5f378e633d55ad9713b84274216a0d1152231
Sniping_Duckling <raineeecarlooo23@gmail.com>
2024-01-06 16:52:12 +0800

    minor updates will add soon
    added few models fixed scenes, will finish how to play scene and questions scene next update

---
+++ Flight-X/Assets/Scripts/SceneMover.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneMover : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Trigger Entered");
+
+        if(other.tag == "Player")
+        {
+            print("Switching Scene to " + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
