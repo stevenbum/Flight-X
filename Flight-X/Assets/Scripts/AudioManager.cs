c0dc4fc441796b9f6bdd4595fc8cf59ab45f7c8b
yantans <129519076+yantans@users.noreply.github.com>
2024-01-07 23:37:51 +0800

    sfx and ui sounds keme
    added a loooooooot, pero magdadagdag pa rin me for character walking naman and etc

---
+++ Flight-X/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource musicSource;
+
+    private void Start()
+    {
+        musicSource = GetComponent<AudioSource>();
+    }
+
+    public void ToggleMusicMute(bool mute)
+    {
+        musicSource.mute = mute;
+        Debug.Log("Music muted: " + mute);
+    }
+}
--- Flight-X/Assets/Scripts/SwitchToggler.cs
+++ Flight-X/Assets/Scripts/SwitchToggler.cs
@@ -7,10 +7,20 @@ using System;
 public class SwitchHandler : MonoBehaviour{
   private int switchState = 1;
   public GameObject switchBtn;
+  public AudioManager audioManager;
   
   public void OnSwitchButtonClicked(){
-    switchBtn.transform.DOLocalMoveX(-switchBtn.transform.localPosition.x,0.2f);
-    switchState = Math.Sign(-switchBtn.transform.localPosition.x);
+        if (switchBtn != null) {
+            switchBtn.transform.DOLocalMoveX(-switchBtn.transform.localPosition.x,0.2f);
+            switchState = Math.Sign(-switchBtn.transform.localPosition.x);
+        }
+        else {
+           Debug.LogError("switchBtn is not assigned. Please assign it in the Unity Editor.");
+        }
+
+        //Toggle music mute based on switch state
+        audioManager.ToggleMusicMute(switchState == -1);
+        Debug.Log("Switch state: " + switchState);
   }
-  
+
 }
