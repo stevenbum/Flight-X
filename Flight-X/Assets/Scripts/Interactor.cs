f701d617de2251b9f0041372402912b29679e53c
Sniping_Duckling <raineeecarlooo23@gmail.com>
2024-01-23 17:48:08 +0800

    Minor Update
    Added Movement Pad, still in early stages same with interact button. Still fixing on how to bind movement to the movement pad. Temporarily the player can move by dragging the left side of the screen and rotating the camera using the right side. Also fixed the UI for Menu, Select Level, and Terms and Conditions. About screen and Instructions screen also needs to be fixed. Will continue to update ASAP.

--- Flight-X/Assets/Scripts/Interactor.cs
+++ Flight-X/Assets/Scripts/Interactor.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 
 interface IInteractable
 {
     public void Interact();
 }
-public class Interactor : MonoBehaviour
+public class Interactor : MonoBehaviour, IPointerDownHandler,IPointerUpHandler
 {
     public Transform InteractorSource;
     public float InteractRange;
+    bool buttonPressed;
 
+    
        //will study how to implement to button already have an idea but will continue to experiment and research to confirm. NGM. Thanks
     void Start()
     {
@@ -20,7 +22,7 @@ public class Interactor : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (buttonPressed)
         {
             Ray r = new Ray(InteractorSource.position, InteractorSource.forward);
                 if(Physics.Raycast(r, out RaycastHit hitInfo, InteractRange))
@@ -32,4 +34,15 @@ public class Interactor : MonoBehaviour
             }
         }
     }
+
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        buttonPressed = true;
+    }
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        buttonPressed = false;
+    }
+
 }
