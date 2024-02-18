229a4175a2314ffa163adbc8a2335abbadc928be
Sniping_Duckling <raineeecarlooo23@gmail.com>
2024-02-18 17:23:39 +0800

    test

--- Flight-X/Assets/Scripts/AudioManager.cs
+++ Flight-X/Assets/Scripts/AudioManager.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     private AudioSource musicSource;
-    private SwitchToggler switchtoggleScript;
+    private SwitchHandler switchtoggleScript;
     private void Start()
     {
         musicSource = GetComponent<AudioSource>();
-
 
     }
 
     public void ToggleMusicMute(bool mute)
     {
+        switchtoggleScript.OnSwitchButtonClicked();
         musicSource.mute = mute;
         Debug.Log("Music muted: " + mute);
     }
